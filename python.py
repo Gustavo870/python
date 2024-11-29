5cb0c6cc0df1c7703be1df5a04fda5aaa378e64a
Gustavo870 <20241TBOR0020012@estudantes.ifpr.edu.br>
2024-11-29 05:33:04 +0000

    A

--- python.py
+++ python.py
@@ -2,4 +2,4 @@ print("Olá Mundo")
 
 nome = input("Qual é o seu nome? ")
 idade = input("Qual é a sua idade? ")
-print(f"Seu nome é (nome) e sua Idade é (idade). Estou correto?")
+print(f"Seu nome é {nome} e sua Idade é {idade}. Estou correto?")
